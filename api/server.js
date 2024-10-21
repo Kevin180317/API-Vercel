3ab6662f91ea844f017701cc2f03d953d209ec67
Superuser <kevin@e-commetrics.com>
2024-10-21 12:38:11 -0700

    fix deploy

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // Definir una ruta que maneje las solicitudes a la raíz
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.status(200).json({ message: "Hello from Vercel Serverless Function!" });
 });
 
@@ -21,8 +21,5 @@ app.get("/jujutsu-kaisen", (req, res) => {
   res.status(200).json(characters);
 });
 
-// Iniciar el servidor en el puerto 3000
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Exportar la aplicación para que Vercel pueda manejarla
+module.exports = app;
