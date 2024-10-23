7e5f632e510479bfab34d106decf35b164546e78
Superuser <kevin@e-commetrics.com>
2024-10-23 16:34:12 -0700

    update version

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Definir una ruta que maneje las solicitudes a la raíz
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from Vercel Serverless Function!" });
+  res.status(200).json({ message: "Hello from Render Express Server!" });
 });
 
 // Definir una ruta que maneje las solicitudes a /jujutsu-kaisen
@@ -16,70 +16,182 @@ app.get("/jujutsu-kaisen", (req, res) => {
       name: "Yuji Itadori",
       cursedTechnique: "Divergent Fist",
       type: "Hechicero",
+      grade: "1",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
     {
       name: "Megumi Fushiguro",
       cursedTechnique: "Ten Shadows Technique",
       type: "Hechicero",
+      grade: "2",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
     {
       name: "Nobara Kugisaki",
       cursedTechnique: "Straw Doll Technique",
       type: "Hechicero",
+      grade: "3",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
-    { name: "Satoru Gojo", cursedTechnique: "Limitless", type: "Hechicero" },
-    { name: "Sukuna", cursedTechnique: "Malevolent Shrine", type: "Maldición" },
+    {
+      name: "Satoru Gojo",
+      cursedTechnique: "Limitless",
+      type: "Hechicero",
+      grade: "Especial",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
+    },
+    {
+      name: "Sukuna",
+      cursedTechnique: "Malevolent Shrine",
+      type: "Maldición",
+      grade: "Especial",
+      affiliation: "N/A",
+    },
     {
       name: "Maki Zenin",
       cursedTechnique: "Heavenly Restriction",
       type: "Hechicero",
+      grade: "4",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
     {
       name: "Toge Inumaki",
       cursedTechnique: "Cursed Speech",
       type: "Hechicero",
+      grade: "2",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
-    { name: "Panda", cursedTechnique: "Gorilla Mode", type: "Hechicero" },
+    {
+      name: "Panda",
+      cursedTechnique: "Gorilla Mode",
+      type: "Hechicero",
+      grade: "2",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
+    },
     {
       name: "Kento Nanami",
       cursedTechnique: "Ratio Technique",
       type: "Hechicero",
+      grade: "1",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
     {
       name: "Mahito",
       cursedTechnique: "Idle Transfiguration",
       type: "Maldición",
+      grade: "Especial",
+      affiliation: "N/A",
     },
-    { name: "Jogo", cursedTechnique: "Disaster Flames", type: "Maldición" },
-    { name: "Hanami", cursedTechnique: "Disaster Plants", type: "Maldición" },
-    { name: "Dagon", cursedTechnique: "Disaster Tides", type: "Maldición" },
-    { name: "Aoi Todo", cursedTechnique: "Boogie Woogie", type: "Hechicero" },
-    { name: "Mai Zenin", cursedTechnique: "Construction", type: "Hechicero" },
+    {
+      name: "Jogo",
+      cursedTechnique: "Disaster Flames",
+      type: "Maldición",
+      grade: "Especial",
+      affiliation: "N/A",
+    },
+    {
+      name: "Hanami",
+      cursedTechnique: "Disaster Plants",
+      type: "Maldición",
+      grade: "Especial",
+      affiliation: "N/A",
+    },
+    {
+      name: "Dagon",
+      cursedTechnique: "Disaster Tides",
+      type: "Maldición",
+      grade: "Especial",
+      affiliation: "N/A",
+    },
+    {
+      name: "Aoi Todo",
+      cursedTechnique: "Boogie Woogie",
+      type: "Hechicero",
+      grade: "1",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
+    },
+    {
+      name: "Mai Zenin",
+      cursedTechnique: "Construction",
+      type: "Hechicero",
+      grade: "3",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
+    },
     {
       name: "Noritoshi Kamo",
       cursedTechnique: "Blood Manipulation",
       type: "Hechicero",
+      grade: "2",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
     },
-    { name: "Choso", cursedTechnique: "Blood Manipulation", type: "Maldición" },
+    {
+      name: "Choso",
+      cursedTechnique: "Blood Manipulation",
+      type: "Maldición",
+      grade: "Especial",
+      affiliation: "N/A",
+    },
     {
       name: "Yuta Okkotsu",
       cursedTechnique: "Rika Orimoto",
       type: "Hechicero",
+      grade: "Especial",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
     {
       name: "Suguru Geto",
       cursedTechnique: "Cursed Spirit Manipulation",
       type: "Hechicero",
+      grade: "Especial",
+      affiliation: "N/A",
     },
     {
       name: "Kasumi Miwa",
       cursedTechnique: "New Shadow Style",
       type: "Hechicero",
+      grade: "3",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
+    },
+    {
+      name: "Kokichi Muta",
+      cursedTechnique: "Puppet Manipulation",
+      type: "Hechicero",
+      grade: "2",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
+    },
+    {
+      name: "Mei Mei",
+      cursedTechnique: "Black Bird Manipulation",
+      type: "Hechicero",
+      grade: "1",
+      affiliation: "N/A",
+    },
+    {
+      name: "Utahime Iori",
+      cursedTechnique: "Unknown",
+      type: "Hechicero",
+      grade: "Semi-1",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
+    },
+    {
+      name: "Yoshinobu Gakuganji",
+      cursedTechnique: "Electric Guitar",
+      type: "Hechicero",
+      grade: "1",
+      affiliation: "Kyoto Metropolitan Magic Technical College",
+    },
+    {
+      name: "Shoko Ieiri",
+      cursedTechnique: "Reverse Cursed Technique",
+      type: "Hechicero",
+      grade: "1",
+      affiliation: "Tokyo Metropolitan Magic Technical College",
     },
   ];
   res.status(200).json(characters);
 });
 
-app.listen(3001, () => console.log("Server ready on port 3001."));
-
-module.exports = app;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
